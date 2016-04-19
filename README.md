4823a2f17b7bff57403ea922ed54560502a6fdf2
Jeffrey Macko <jeffrey@macko.fr>
2016-04-19 18:02:14 +0200

    First Commit

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # privoxy-bitrise
 
 A step who act as a proxy to help analyze requests made by the simulator during integration.
+
+# To Do
+
+- eee
+
+# Milestone
+
+* 0.1.0
+
+- eee
+
+* 1.0.0
+
+- eee
---
+++ step.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+proxy_url="127.0.0.1"
+proxy_port="8142"
+privoxy_logfile="/usr/local/var/log/privoxy/logfile"
+privoxy_configfile="privoxy_configfile"
+
+# Configs
+echo ""
+echo "========== Configs =========="
+echo "proxy: ${proxy_url}:${proxy_port}"
+echo "logfile: ${logfile}"
+if [[ -n "${fauxpas_debug_mode}" ]]; then
+	echo "fauxpas_debug_mode: ${fauxpas_debug_mode}"
+fi
+echo "============================="
+echo ""
+
+if [[ "${fauxpas_debug_mode}" = true ]]; then
+	set -x
+fi
+
+brew install privoxy
+
+ln -sfv /usr/local/opt/privoxy/*.plist ~/Library/LaunchAgents
+privoxy_bin=$(/usr/libexec/PlistBuddy -c "Print:ProgramArguments:0" ~/Library/LaunchAgents/homebrew.mxcl.privoxy.plist)
+# privoxy_config=$(/usr/libexec/PlistBuddy -c "Print:ProgramArguments:2" ~/Desktop/homebrew.mxcl.privoxy.plist)
+sudo networksetup -setwebproxy "Ethernet 1" ${proxy_url} ${proxy_port}
+eval "$(privoxy_bin) $(privoxy_configfile)"
+
+echo ""
+echo "========== Outputs =========="
+echo "PRIVOXY_LOG: ${logfile}"
+echo "============================="
+echo ""
+
+exit 0
